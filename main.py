0fcb2157f247083bb006c7bc0fef02998df03ce7
DanR2468 <dr555@cam.ac.uk>
2022-11-21 12:22:38 +0000

    cleaning up code and adding comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from numpy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 import scipy.signal.windows as window
 
+# Function used to generate completely random signal without resonance frequencies
 """
 def generate_random_signal(fs,N,start,end, weight_constant=False):
     T = (end - start) / N
@@ -21,54 +22,52 @@ def generate_random_signal(fs,N,start,end, weight_constant=False):
 """
 
 def plot_signal(t_show, x_show, x_true=None):
-    #plt.scatter(t_show, x_show, c='y', edgecolors='r')
-    if x_true is not None: plt.plot(t_show, x_true, 'g', label="Signal")
-    plt.plot(t_show, x_show, '--' 'b', label="New Signal")
-    x_show = np.tile(window.tukey(int(x_show.size/N)), N)        # display window function used
-    plt.plot(t_show, x_show, 'orange', label="Window Function")
+    if x_true is not None: plt.plot(t_show, x_true, 'g', label="Signal")            # Plot the sampled signal
+    plt.plot(t_show, x_show, '--' 'b', label="New Signal")                          # Plot the corrected signal
+    x_show = np.tile(window.blackman(int(x_show.size/N)), N)
+    plt.plot(t_show, x_show, 'orange', label="Window Function")                     # Plot the window function used
     plt.legend()
     plt.xlabel("Time")
     plt.ylabel("Amplitude")
+    plt.grid()
+    plt.show()
 
 def plot_frequency(arrays, fs):
     for array in arrays:
-        X_show = np.abs(fft(array))
-        freq = fftfreq(x_show.size, 1 / fs)
-        plt.plot(freq, X_show)
+        X_show = np.abs(fft(array))                                                 # Perform fast fourier transform on array
+        freq = fftfreq(x_show.size, 1 / fs)                                         # Find corresponding frequencies
+        plt.plot(freq, X_show)                                                      # Plot frequency spectrum
+    plt.xlabel("Frequency")
+    plt.ylabel("Amplitude")
+    plt.grid()
+    plt.show()
 
-def generate_signal(sines, fs,N,start,end,weight_constant=False, to_plot=True):
-    T = (end - start) / N
-    t_show = np.arange(start, end, 1 / fs)
-    t = np.arange(0, T, 1 / fs)
-    x=0
-    x_true=0
-    if weight_constant:
+def generate_signal(sines, fs, N, start, end, weight_constant=False, to_plot=True):
+    t_show = np.arange(start, end, 1 / fs)                                          # Create array of x axis values
+    t = np.arange(0, (end - start) / N, 1 / fs)                                     # Split sampled period into time intervals
+    x = 0                                                                           # Initialize arrays
+    x_true = 0
+    if weight_constant:                                                             # If varying amplitudes are desired
         for sine in sines:
-            A, PHASE = np.random.normal(1,0.25), 2*np.pi*np.random.random()
-            x += A * np.sin(sine * t + PHASE )
-            #x_true += A * np.sin(sine * t_show + PHASE )
-    else:
+            A, PHASE = np.random.normal(1,0.25), 2 * np.pi * np.random.random()     # Generate random amplitude and phase
+            x += A * np.sin(sine * t + PHASE)                                       # Add array of values for each resonance frequency
+    else:                                                                           # If constant amplitude is desired
         for sine in sines:
-            PHASE = 2*np.pi*np.random.random()
-            x += np.sin(sine * t + PHASE )
-            #x_true += np.sin(sine * t_show + PHASE )
-    x_true = np.tile(x, N)                          # Original randomized signal sampled for period T
-    x = np.multiply(x, window.tukey(x.size))     # Original signal multiplied by window function
+            PHASE = 2 * np.pi * np.random.random()                                  # Generate random phase
+            x += np.sin(sine * t + PHASE)                                           # Add array of values for each resonance frequency
+    x_true = np.tile(x, N)                                                          # Original randomized signal sampled for period T
+    x = np.multiply(x, window.blackman(x.size))                                     # Original signal multiplied by window function
     x_show = np.tile(x, N)
     if to_plot: return t_show, x_show, x_true
     else: return t, x, x_true
 
 
 if __name__ =='__main__':
-    fs = 28.0 # Sample and evaluate the data at this frequency for the period T
-    N = 6
-    start = 0
-    end = 30.0
-    resonance= 2* np.pi *np.array([2.23, 1.03,3.13])
-    t_show, x_show, x_true = generate_signal(resonance,fs,N,start,end,True)
-    plot_signal(t_show, x_show, x_true)
-    plt.grid()
-    plt.show()
-    plot_frequency([x_true, x_show],fs)
-    plt.grid()
-    plt.show()
+    fs = 28.0                                                                       # Sample and evaluate the data at this frequency for the period T
+    N = 6                                                                           # Number of repetitions of sample
+    start = 0                                                                       # Start time
+    end = 30.0                                                                      # End time
+    resonance= 2 * np.pi * np.array([2.23, 1.03, 3.13])                             # Resonance frequencies of structure
+    t_show, x_show, x_true = generate_signal(resonance, fs, N, start, end, True)    # Generate random signal made up of resonance frequencies with random phases and amplitudes
+    plot_signal(t_show, x_show, x_true)                                             # Plot random, corrected and window function signals
+    plot_frequency([x_true, x_show], fs)                                            # Plot frequency spectrum
--- window_functions.py
+++ window_functions.py
@@ -2,19 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal.windows as window
 
-def Hann_function(N=None, t=None):
-    if N is None: N=t.size
-    f = np.arange(t.size)
-    f = 1/2 * (1-np.cos( 2*np.pi* f /(N-1) ) )
-    if t is not None: f *= t
-    return f
-
-#def Tukey_function(N=None, t=None, a=0.5):
-
 if __name__ =='__main__':
-    t=np.arange(0, 25)
+    t = np.arange(0, 100)
     plt.plot(t, window.hann(t.size), label="hann")
     plt.plot(t, window.tukey(t.size), label="tukey")
+    plt.plot(t, window.blackman(t.size), label="Blackman")
     plt.plot(t, window.kaiser(t.size, 3), label="kaiser")
     plt.legend()
     plt.show()
